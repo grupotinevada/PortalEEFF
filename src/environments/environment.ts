e4ac82afbc4752dff05ab053e9c5b07e1f19156e
Nacholoz16 <ig.salazar@duocuc.cl>
2025-11-25 10:47:02 -0300

    feat: Update spinner component styles and HTML structure; modify environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://portaleeffapi.inevada.cl/api',
-  // loginbutton:'https://portaleeffapi.inevada.cl/api/auth/microsoft'
-  apiUrl: 'https://portaleeffapi.inevada.cl/api', 
+  apiUrl: 'https://portaleeffapi.inevada.cl/api',
   loginbutton:'https://portaleeffapi.inevada.cl/api/auth/microsoft'
+  //   apiUrl: 'https://portaleeffapitest.inevada.cl/api', // Ejemplo de variable
+  // loginbutton:'https://portaleeffapitest.inevada.cl/api/auth/microsoft'
 };
